9cdc3591677b66801b1eb4b28621483ecc58f0f7
Layne <fdgod5@163.com>
2022-09-28 11:41:34 +0800

    2022/9/28 commit

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -26,21 +26,26 @@ export default {
   },
   methods: {
     // 借助mapMutations生成对应的方法，方法中会调用commit去联系mutations(对象写法)
-    ...mapMutations({ increment: 'JIA', decrement: 'JIAN' }),
+    ...mapMutations('countOptions', { increment: 'JIA', decrement: 'JIAN' }),
     // 借助mapMutations生成对应的方法，方法中会调用commit去联系mutations(数组写法)
     // ...mapMutations(['JIA', 'JIAN']),
 
     // 借助mapActions生成对应的方法
-    ...mapActions({ incrementOdd: 'jiaOdd', incrementWait: 'jiaWait' }),
+    ...mapActions('countOptions', {
+      incrementOdd: 'jiaOdd',
+      incrementWait: 'jiaWait',
+    }),
   },
   computed: {
     // 借助mapGetter生成计算属性，从getters中读取数据(数组写法)
-    ...mapGetters(['bigSum']),
+    ...mapGetters('countOptions', ['bigSum']),
     // 借助mapState生成计算属性， 从state中读取数据（对象写法）
     // ...mapState({ sum: 'sum', subject: 'subject', school: 'school' }),
 
     // 借助mapState生成计算属性， 从state中读取数据（数组写法）
-    ...mapState(['sum', 'subject', 'school', 'personList']),
+    ...mapState('countOptions', ['sum', 'subject', 'school']),
+    ...mapState('personOptions', ['personList']),
+    // ...mapState(['countOptions', 'personOptions']),
   },
   mounted() {
     const x = mapState({
--- src/store/index.js
+++ src/store/index.js
@@ -4,59 +4,16 @@
 import Vue from 'vue'
 // 引入Vuex
 import Vuex from 'vuex'
-
+import countOptions from './count'
+import personOptions from './person'
 // 求和相关的设置
-const countOptions = {
-  actions: {
-    jiaOdd(context, value) {
-      if (context.state.sum % 2) {
-        context.commit('JIA', value)
-      }
-    },
-    jiaWait(context, value) {
-      setTimeout(() => {
-        context.commit('JIA', value)
-      }, 1000)
-    },
-  },
-  mutations: {
-    JIA(state, value) {
-      state.sum += value
-    },
-    JIAN(state, value) {
-      state.sum -= value
-    },
-  },
-  state: {
-    sum: 0, // 当前的和
-    school: '清华',
-    subject: '前端',
-  },
-  getters: {
-    bigSum(state) {
-      return state.sum * 10
-    },
-  },
-}
-// 人员管理相关的设置
-const personOptions = {
-  actions: {},
-  mutations: {
-    ADD_PERSON(state, personObj) {
-      state.personList.unshift(personObj)
-    },
-  },
-  state: {
-    personList: [{ id: '001', name: '张三' }],
-  },
-  getters: {},
-}
 
+// 人员管理相关的设置
 Vue.use(Vuex)
 // 创建并导出store
 export default new Vuex.Store({
   modules: {
-    a: countOptions,
-    b: personOptions,
+    countOptions,
+    personOptions,
   },
 })
